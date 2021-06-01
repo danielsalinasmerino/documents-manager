08ff73f1496f6135210fb8c9ad9a9862ae9548d1
Daniel Salinas Merino <daniel.salinas@wondo.es>
2021-06-01 09:53:00 +0200

    fix: Ports cleaning

--- back/environment.js
+++ back/environment.js
@@ -4,7 +4,7 @@
 const dockerDeploy = true; //change to false when you are working on local 
 
 const variables = {
-    port: 3000,
+    port: 3009,
     connection: (dockerDeploy) ? ('mongodb://mongo:27017/documents-manager') : ('mongodb://localhost:27017/documents-manager'),
     //nameOfMongoInstance: mongo (YOU CAN CHECK THIS ON docker-compose.yml)
     //mongoPort: 27017 (YOU CAN CHECK THIS ON docker-compose.yml)
