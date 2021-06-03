ff5bbc2dfd1c8727efc13da703a44d17e6db85fe
Daniel Salinas Merino <daniel.salinas@wondo.es>
2021-06-03 13:09:43 +0200

    feat(front/src/components/edition-buttons-menu-component/EditionButtonsMenuComponent.js): Minor change on buttons order

--- front/src/components/edition-buttons-menu-component/EditionButtonsMenuComponent.js
+++ front/src/components/edition-buttons-menu-component/EditionButtonsMenuComponent.js
@@ -15,11 +15,11 @@ function EditionButtonsMenuComponent({ openModalCallback }) {
     return (
         <div className="buttonsMenu editionView">
             <StyledButtonComponent clickButton={openModalCallback} buttonText={'Añadir Sección'}/>
-            <Link className="link-clean" to={"/" + portalName + "/gestor-documental/vista-previa"}>
-                <StyledButtonComponent buttonText={'Vista Previa'}/>
-            </Link>
             <Link className="link-clean" to={"/" + portalName + "/gestor-documental/gestion-editores"}>
                 <StyledButtonComponent buttonText={'Gestión de Editores'}/>
+            </Link>
+            <Link className="link-clean" to={"/" + portalName + "/gestor-documental/vista-previa"}>
+                <StyledButtonComponent buttonText={'Vista Previa'}/>
             </Link>
         </div>
     );
