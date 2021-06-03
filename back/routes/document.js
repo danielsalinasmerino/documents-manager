010e9ac7a5f33c40f172c41ab6efe76af1ea1646
Daniel Salinas Merino <daniel.salinas@wondo.es>
2021-06-03 13:00:57 +0200

    fix: Fix on routing to delete

--- back/routes/document.js
+++ back/routes/document.js
@@ -14,7 +14,7 @@ router.post(urlStart, DocumentController.createDocument);
 router.get(urlStart, DocumentController.readDocuments);
 router.get((urlStart + '/:idDocument'), DocumentController.readDocumentById);
 router.put((urlStart + '/:idDocument'), DocumentController.updateDocumentById);
-router.delete((urlStart + '/:idDocument'), DocumentController.deleteDocumentById);
+router.delete((urlStart + '/delete/:idDocument'), DocumentController.deleteDocumentById);
 router.delete((urlStart + '/deleteAll'), DocumentController.deleteDocuments); 
 
 module.exports = router;
