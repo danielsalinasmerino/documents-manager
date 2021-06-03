e1be4dfaa22d1ec0f85258173a3ea3847dcc041f
Daniel Salinas Merino <daniel.salinas@wondo.es>
2021-06-03 13:02:06 +0200

    feat: Changed Front endpoints to delete elements

--- front/src/services/endpoints.js
+++ front/src/services/endpoints.js
@@ -9,10 +9,10 @@ export const readSectionsEndpoint = endpointStart + "sections";
 //export const readSectionsPdiEndpoint = endpointStart + "sections/pdi";
 //export const readSectionsEstudiantesEndpoint = endpointStart + "sections/estudiantes";
 export const updateSectionByIdEndpoint = endpointStart + "sections";
-export const deleteSectionByIdEndpoint = endpointStart + "sections";
+export const deleteSectionByIdEndpoint = endpointStart + "sections/delete";
 
 // Documents related endpoints
 export const createDocumentEndpoint = endpointStart + "documents";
 export const readDocumentsEndpoint = endpointStart + "documents";
 export const updateDocumentByIdEndpoint = endpointStart + "documents";
-export const deleteDocumentByIdEndpoint = endpointStart + "documents";
+export const deleteDocumentByIdEndpoint = endpointStart + "documents/delete";
